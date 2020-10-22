0bdcc720e652fa88bdf7ae58525f43224cce85a5
jj bernal <beastbernal@gmail.com>
2020-10-21 19:30:20 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
+import headerLogo from './linkedin.jpg';
 import './App.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee, faOilCan } from '@fortawesome/free-solid-svg-icons'
+// import Viewpager from './Viewpager'
+import TitlebarGridList from './TitlebarGridList'
+import { makeStyles } from '@material-ui/core/styles';
+import { Alert, AlertTitle } from '@material-ui/lab';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+    <div >
+      <header className="banner-background">
+        
+        <h1 className="h1-header"> Juan Jose Bernal Villamarín</h1>
+        <h6 className="h5-header"> Senior Front End Developer | Senior JS Developer | Senior Fullstack Developer | Front End Developer Architect | Technical Lead</h6>
+        
       </header>
+      {/* <Viewpager></Viewpager> */}
+      
+
     </div>
+      <div className={classes.root}>
+        <Alert severity="success">
+          <AlertTitle>About me</AlertTitle>
+          I am an engineer who has worked in government entities analyzing, designing and implementing business solutions that are still in intensive use, my ability to solve problems is present in the productive or organizational systems and the development of attributes and skills such as leadership, self-training capacity, <strong>ability to work in a team and personal values make me an integral professional.</strong>
+        </Alert>
+      </div>
+    
+      <TitlebarGridList></TitlebarGridList>
+    </>
   );
 }
 
---
+++ src/TitlebarGridList.jsx
@@ -0,0 +1,236 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from "@material-ui/core/IconButton";
+import InfoIcon from "@material-ui/icons/Info";
+import Lottie from "react-lottie";
+import educationData from "./assets/book-loading.json";
+import projData from "./assets/developer.json";
+import medalData from "./assets/medal-animation.json";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGraduationCap, faLaptopCode, faMedal } from '@fortawesome/free-solid-svg-icons'
+
+
+// import tileData from './tileData';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: "95%",
+    height: "100%",
+    paddingBottom: '200px'
+  },
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)",
+  },
+}));
+
+/**
+ * The example data is structured as follows:
+ *
+ * import image from 'path/to/image.jpg';
+ * [etc...]
+ *
+ * const tileData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ */
+const tileData = [
+  {
+    title: "Technology Management Specialist - 2012",
+    subtitle: "Bogotá-Col	Universidad Autónoma de Colombia",
+  },
+  {
+    title: "Telematic Engineering - 2005",
+    subtitle: "Bogotá-Col	Universidad Distrital Francisco José de Caldas",
+  },  
+  {
+    title: "Data Systematization Technology - 2003",
+    subtitle: "Bogotá-Col	Universidad Distrital Francisco José de Caldas",
+  },
+  
+];
+
+const jobData = [
+  {
+    title: "FISCALÍA GENERAL DE LA NACIÓN - [2016 - nowadays] ",
+    subtitle: "Bogotá-Col	•	Front-End Developer (Vuejs, React, Angular, Bootstrap). ",
+  },
+  {
+    title: "GGT INFORMÁTICA  - 2016",
+    subtitle: "Bogotá-Col	•	Front-End Developer (Angular, Bootstrap).",
+  },  
+  {
+    title: "Dirección de Impuestos y Aduanas Nacionales – DIAN - [2005 - 2015]",
+    subtitle: "Bogotá-Col	•	Front-End Developer (Flex, JavaScript, JSF, RichFaces).",
+  },
+  
+];
+
+const certificationData = [
+  {
+    title: "Front-end avanzado en React - 2020 ",
+    subtitle: "Bogotá-Col Acámica",
+  },
+  {
+    title: "Curso Profesional de JavaScript - 2020",
+    subtitle: "Platzi Credential ID  e4a6406f-cbee-4cc6-ad01-aae3a44cc648",
+  },  
+  {
+    title: "Curso de Asincronismo con JavaScript - 2020",
+    subtitle: "Platzi Credential ID  e4bc353b-2aa7-4047-b71d-c86478e4a49a",
+  },  
+  {
+    title: "Curso de ECMAScript 6",
+    subtitle: "Platzi Credential ID  3a361b0a-f27d-4bbe-ae69-cf0c162345d3",
+  },  
+  {
+    title: "Curso Básico de JavaScript",
+    subtitle: "Platzi Credential ID  d9a69bbd-b604-40f8-ab95-262907c84043",
+  },  
+  {
+    title: "Fundamentos de JavaScript",
+    subtitle: "Platzi Credential ID  09fc544d-bbc2-452f-b241-cc4bf9442737",
+  },  
+  
+];
+
+export default function TitlebarGridList() {
+  const classes = useStyles();
+
+  const educationOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: educationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const projOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: projData,
+    
+  };
+
+  const medalOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: medalData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div className={classes.root}>
+      <GridList className={classes.gridList} cols={1} cellHeight={"60vh"}>
+        <GridListTile>
+        {tileData.map((tile) => (
+        <Card className={classes.root} variant="outlined" key={tile.title}>
+            <CardMedia>
+              <Lottie options={educationOptions} height={100} width={100} style={{'left': 40}} />
+            </CardMedia>
+            <div className={classes.details}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {tile.title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {tile.subtitle}
+                </Typography>
+              </CardContent>
+            </div>
+          </Card>
+        ))}
+
+          <GridListTileBar
+            style={{'marginTop': '30px'}}
+            title="EDUCATION"
+            actionIcon={
+              <FontAwesomeIcon icon={faGraduationCap}  size="lg"/>
+            }
+          />
+        </GridListTile>
+
+        <GridListTile>
+        {jobData.map((tile) => (
+        <Card className={classes.root} variant="outlined" key={tile.title}>
+            <CardMedia>
+              <Lottie options={projOptions} height={100} width={100} />
+            </CardMedia>
+            <div className={classes.details}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {tile.title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {tile.subtitle}
+                </Typography>
+              </CardContent>
+            </div>
+          </Card>
+        ))}
+
+          <GridListTileBar
+            title="PROFESSIONAL EXPERIENCE"
+            actionIcon={
+              <FontAwesomeIcon icon={faLaptopCode}  size="lg"/>
+            }
+          />
+        </GridListTile>
+        <GridListTile>          
+        {certificationData.map((tile) => (
+        <Card className={classes.root} variant="outlined" key={tile.title}>
+            <CardMedia>
+              <Lottie options={medalOptions} height={100} width={100} />
+            </CardMedia>
+            <div className={classes.details}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {tile.title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {tile.subtitle}
+                </Typography>
+              </CardContent>
+            </div>
+          </Card>
+        ))}
+
+          <GridListTileBar
+            title="Certifications"
+            actionIcon={
+              <FontAwesomeIcon icon={faMedal}  size="lg"/>
+            }
+          />
+        </GridListTile>
+      </GridList>
+    </div>
+  );
+}
